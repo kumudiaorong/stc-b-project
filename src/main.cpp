f52590e0a226d7ba30102ae4f2638b0a5c45ce41
kumudiaorong <kmdr.error@gmail.com>
2023-09-06 09:25:53 +0800

    test uart

--- src/main.cpp
+++ src/main.cpp
@@ -6,39 +6,34 @@
 // 2: 外部IO的中断
 #define EXT_ISR_MODE 1
 
-void TouchEvent()
-{
-    Serial.printf("Touch Event.\r\n");
+void TouchEvent() {
+  Serial.printf("Touch Event.\r\n");
 }
 
-void PinIntEvent()
-{
-    Serial.printf("PinInt Event.\r\n");
+void PinIntEvent() {
+  Serial.printf("PinInt Event.\r\n");
 }
 
-void setup()
-{
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  // Serial1.begin(9600);
 #if 1 == EXT_ISR_MODE
-    // Pin: T0(GPIO4), 函数指针:TouchEvent, 阈值: 40
-    touchAttachInterrupt(T0, TouchEvent, 40);
-
+  // Pin: T0(GPIO4), 函数指针:TouchEvent, 阈值: 40
+  // touchAttachInterrupt(T0, TouchEvent, 40);
 #elif 2 == EXT_ISR_MODE
-    pinMode(0, INPUT_PULLUP);
-    attachInterrupt(0, PinIntEvent, FALLING);
+  pinMode(0, INPUT_PULLUP);
+  attachInterrupt(0, PinIntEvent, FALLING);
 
 #endif
 }
 
-void loop()
-{
-    // put your main code here, to run repeatedly:
+void loop() {
+  // put your main code here, to run repeatedly:
 
 #if 0 == EXT_ISR_MODE
     Serial.printf("touch:%d\r\n", touchRead(T0));
 #endif
-
-    delay(200);
+  Serial.print(0x11);
+  delay(2000);
 }
